7edf78b6a68768bab32bd13e34178056ce86e381
talgat <15talgat@mail.ru>
2022-12-22 13:36:30 +0300

    done zadacha_55

---
+++ zadacha_55/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 55: Задайте двумерный массив. 
+// Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void ReverseString(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++) // Пошли по строкам arr.GetLength(0)
+    {
+        for (int j = 0; j < i; j++) // j < i потому что мы идем  по строкам. Идем до диагонали.
+        {
+            int temp = arr[i,j];
+            arr[i,j] = arr[j,i];
+            arr[j,i] = temp;
+        }
+    }
+    
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+int EnterNUmber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int m = EnterNUmber("Введите значение M: ");
+int n = EnterNUmber("Введите значение N: ");
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+ShowArray(matrix);
+ReverseString(matrix);
+System.Console.WriteLine();
+ShowArray(matrix);
