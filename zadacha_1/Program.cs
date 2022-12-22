25b56f10b9eac2070ff7f2135af42d20535d7143
talgat <15talgat@mail.ru>
2022-12-22 13:36:11 +0300

    done zadacha_1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+
+void ReverseString(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        {
+            int temp = arr[0, i];
+            arr[0, i] = arr[arr.GetLength(0) - 1, i];
+            arr[arr.GetLength(0) - 1, i] = temp;
+        }
+    }
+}
+
+void ShowArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+int EnterNUmber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int m = EnterNUmber("Введите значение M: ");
+int n = EnterNUmber("Введите значение N: ");
+int[,] matrix = CreateRandomArray(m, n, 1, 10);
+ShowArray(matrix);
+ReverseString(matrix);
+System.Console.WriteLine();
+ShowArray(matrix);
